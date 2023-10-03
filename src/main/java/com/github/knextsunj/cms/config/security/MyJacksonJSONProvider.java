5b74a9781dacf85d7d10f4a91106c2aebaee05cd
knextsunj <knextsunj@users.noreply.github.com>
2023-10-04 02:00:33 +0530

    tomee-hibernate migration

---
+++ src/main/java/com/github/knextsunj/cms/config/security/MyJacksonJSONProvider.java
@@ -0,0 +1,14 @@
+package com.github.knextsunj.cms.config.security;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class MyJacksonJSONProvider extends JacksonJsonProvider {
+}
